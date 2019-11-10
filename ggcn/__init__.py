608d642ea66fa5d1efa97ab9b9cbbe2d2657b078
Kautenja <kautencreations@gmail.com>
2019-11-10 14:32:02 -0600

    cleanupp

---
+++ ggcn/__init__.py
@@ -0,0 +1,9 @@
+"""
+Gaussian graph convolution layer.
+
+Reference:
+    Authors: Dingyuan Zhu, Ziwei Zhang, Peng Cui, Wenwu Zhu
+    Title: Robust Graph Convolutional Networks Against Adversarial Attacks
+    URL: https://doi.org/10.1145/3292500.3330851
+
+"""
---
+++ ggcn/losses.py
@@ -0,0 +1,9 @@
+"""
+losses for Gaussian graph convolution neural networks.
+
+Reference:
+    Authors: Dingyuan Zhu, Ziwei Zhang, Peng Cui, Wenwu Zhu
+    Title: Robust Graph Convolutional Networks Against Adversarial Attacks
+    URL: https://doi.org/10.1145/3292500.3330851
+
+"""
